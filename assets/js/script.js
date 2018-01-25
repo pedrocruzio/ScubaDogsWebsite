fbf4136da0b256eb38f4f2ae446d09613f98ffc5
Fabian <fabian.velez1996@gmail.com>
2018-01-24 23:14:26 -0400

    Most Issues Fixed!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,53 @@
+$("#owl-demo").owlCarousel({
+    navigation : true
+  });
 
-  $('.offers').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true
-      },
-      600: {
-        items: 3,
-        nav: true
-      },
-      1000: {
-        items: 5,
-        nav: true,
-        loop: true
-      }
+$('.partners').owlCarousel({
+  loop: true, margin: 10,
+  // autoplay: true,
+  autoplayHoverPause: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+      loop: true
+    },
+    800: {
+      items: 3,
+      nav: true,
+      loop: true
+    },
+    1200: {
+      items: 4,
+      nav: true,
+      loop: true
     }
-  })
+  }
+})
 
-  $('.partners').owlCarousel({
-    loop: true,
-    margin: 10,
-    autoplay: true,
-    autoplayHoverPause: true,
-
-    responsive: {
-      0: {
-        items: 1,
-        nav: true,
-        loop: true
-      },
-      600: {
-        items: 3,
-        nav: true,
-        loop: true
-      },
-      1000: {
-        items: 5,
-        nav: true,
-        loop: true
-      }
+$('.offers').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  navText: [
+    "<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true
+    },
+    600: {
+      items: 2,
+      nav: true
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: true
     }
-  })
+  }
+})
